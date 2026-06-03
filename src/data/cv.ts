2fb77807e1913da960faaf540d41f1ab4f4bf220
yuting <yuting@yutingtekiMacBook-Air.local>
2026-06-02 22:41:53 -0600

    cv

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,63 +1,147 @@
 export const experiences = [
-	{
-		company: '',
-		time: '',
-		title: '',
-		location: '',
-		description: '',
-	},
-	// {
-	// 	company: 'Radium Institute (Institut du Radium)',
-	// 	time: '1914 - 1934',
-	// 	title: 'Director',
-	// 	location: 'Paris, France',
-	// 	description: 'Led groundbreaking studies on radioactivity and mentored future Nobel Prize laureates.',
-	// },
+  {
+    title: "English teacher (intern)",
+    company: "Yancheng Tinghu Junior High School ",
+    time: "2022.09--2022.11",
+    description: "Teach English to junior high school students",
+  },
+  {
+    title: "English teacher (intern)",
+    company: "Yancheng No.1 Senior High School ",
+    time: "2023.01--2023.04",
+    description: "Teach English to senior high school students",
+  },
+  {
+    title: "Research Assistant (RA)",
+    company: "The Centre for Language Studies (CLS) at Radboud University",
+    time: "2025.01--2025.03",
+    description: "VR experiment RA",
+  },
+  {
+    title: "Research Internship ",
+    company: "Donders Institute for Brain, Cognition, and Behaviour ",
+    time: "2024.08--2025.07",
+    description: "Master internship, design experiment and collect data",
+  },
+  {
+    title: "Graduate Teaching Assistant(GTA) ",
+    company: "University of Alberta",
+    time: "2025.09--2025.12",
+    description: "TA for Ling101",
+  },
+  {
+    title: "Graduate Teaching Assistant(GTA) ",
+    company: "University of Alberta",
+    time: "2026.01--2026.04",
+    description: "TA for Ling101",
+  },
+  {
+    title: "K-9 Technical Advisement",
+    company: "Alberta Machine Intelligence Institute (Amii)",
+    time: "2026.05.01--2026.05.10",
+    description:
+      "Visit 5 schools and teach AI-related courses to K-9 students ",
+  },
+  {
+    title: "Graduate Research Assistant(GRA) ",
+    company: "University of Alberta",
+    time: "2026.05--2026.08",
+    description: "Annotate and analyze speech data",
+  },
 ];
 
 export const education = [
-	{
-		school: '',
-		time: '',
-		degree: '',
-		location: '',
-		description: '',
-	},
-	// {
-	// 	school: 'University of Paris',
-	// 	time: '1891 - 1895',
-	// 	degree: 'Master’s in Physics and Mathematics',
-	// 	location: 'Paris, France',
-	// 	description: 'Graduated at the top of her class in physics and second in mathematics.',
-	// },
+  {
+    school: "Yangzhou University",
+    time: "2019.09--2023.06",
+    degree: "Bachelor of Art(BA), English(education)",
+    location: "Yangzhou, China",
+  },
+  {
+    school: "Tohoku University",
+    time: "2022. 04--2022.08",
+    degree: "Exchange Program, IPLA",
+    location: "Sendai, Japan",
+  },
+  {
+    school: "Leiden University",
+    time: "2023.09--2025.07",
+    degree: "Master of Art(MA), Linguistics",
+    location: "Leiden, the Netherlands",
+  },
+  {
+    school: "University of Alberta",
+    time: "2025.09--",
+    degree: "PhD student in Linguistics",
+    location: "Edmonton, Canada",
+  },
 ];
 
-export const skills = [
-	{
-		title: '',
-		description: '',
-	},
-	// {
-	// 	title: 'Experimental Techniques',
-	// 	description: 'Spectroscopy, Isolation of Radioactive Elements, Radiation Measurement',
-	// },
+export const Honours = [
+  {
+    title: "GSA Academic Travel Grant ($500)",
+    time: "2026.03",
+    description: "University of Alberta",
+  },
+  {
+    title: "Graduate Recruitment Award($5000)",
+    time: "2025.09",
+    description: "University of Alberta",
+  },
+  {
+    title: "Exchange Programm Scholarship",
+    time: "2022.04",
+    description: "Yangzhou University",
+  },
+  {
+    title: "Academic Scholarship",
+    time: "2021.12",
+    description: "Yangzhou University",
+  },
+  {
+    title: "Outstanding Member of Student Associations,",
+    time: "2020.05",
+    description: "Yangzhou University",
+  },
 ];
 
 export const publications = [
-	{
-		title: '',
-		authors: '',
-		journal: '',
-		time: '',
-		link: '',
-		abstract: '',
-	},
-	// {
-	// 	title: 'The Radiation of Uranium Compounds',
-	// 	authors: 'Marie Curie',
-	// 	journal: 'Comptes Rendus de l’Académie des Sciences',
-	// 	time: '1898',
-	// 	link: '#',
-	// 	abstract: 'Early research leading to the identification of uranium’s radioactive properties.',
-	// },
+  {
+    title: "",
+    authors: "",
+    journal: "",
+    time: "",
+    link: "",
+    abstract: "",
+  },
+];
+
+export const Conference = [
+  {
+    title:
+      "Longitudinal Acoustic Analysis of the English Consonant /ɹ/ in the Word Frog Among Japanese Returnee Children",
+    authors: "Yuting Sun, Tim Laméris",
+    description: "Poster Presentation",
+    name: "15th International Conference of Experimental Linguistics (ExLing2024)",
+    location: "Paris, France",
+    time: "2024.10",
+  },
+  {
+    title:
+      "The Impact of Proficiency and Word Type on L2 Learners: A Picture Naming Task",
+    authors: "Yuting Sun",
+    description: "Poster Presentation",
+    name: "TABU Dag 2025",
+    location: "Groningen, the Netherlands",
+    time: "2025.06",
+  },
+  {
+    title:
+      "Focus on Canadian English: Acoustic Realization and Listener Perception",
+    authors: "Yuting Sun, Ziyan Zou, Anja Arnhold",
+    description: "Poster Presentation",
+    name: "LabPhon20",
+    location: "Montreal, Canada",
+    time: "2026.06",
+  },
 ];
